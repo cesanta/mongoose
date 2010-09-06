236768b30ca63c9f9ab6c59edb98902fae7e8225
valenok <devnull@localhost>
2010-09-06 21:46:07 +0100

    2.9 -> 2.10

--- mongoose.c
+++ mongoose.c
@@ -198,7 +198,7 @@ typedef int SOCKET;
 
 #include "mongoose.h"
 
-#define MONGOOSE_VERSION "2.9"
+#define MONGOOSE_VERSION "2.10"
 #define PASSWORDS_FILE_NAME ".htpasswd"
 #define CGI_ENVIRONMENT_SIZE 4096
 #define MAX_CGI_ENVIR_VARS 64
