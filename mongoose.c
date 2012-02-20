b10b989865a830a03cdeb7ef06336401cd0cb380
Sergey Lyubka <valenok@gmail.com>
2012-02-20 23:27:40 +0000

    Increasing master thread priority on Windows

--- mongoose.c
+++ mongoose.c
@@ -4029,13 +4029,15 @@ static void master_thread(struct mg_context *ctx) {
   struct socket *sp;
   int max_fd;
 
-#if defined(ISSUE_317)
   // Increase priority of the master thread
+#if defined(_WIN32)
+  SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_ABOVE_NORMAL);
+#endif
+  
+#if defined(ISSUE_317)
   struct sched_param sched_param;
-  int policy;
-  pthread_getschedparam(pthread_self(), &policy, &sched_param);
-  sched_param.sched_priority = sched_get_priority_max(policy);
-  pthread_setschedparam(pthread_self(), policy, &sched_param);
+  sched_param.sched_priority = sched_get_priority_max(SCHED_RR);
+  pthread_setschedparam(pthread_self(), SCHED_RR, &sched_param);
 #endif
 
   while (ctx->stop_flag == 0) {
