ef4bd8f75838e2b3f45917662e587efadbf0eb61
Sergey Lyubka <valenok@gmail.com>
2012-06-04 19:08:43 +0100

    Setting content-length

--- examples/hello.c
+++ examples/hello.c
@@ -6,11 +6,17 @@ static void *callback(enum mg_event event,
                       struct mg_connection *conn,
                       const struct mg_request_info *request_info) {
   if (event == MG_NEW_REQUEST) {
-    // Echo requested URI back to the client
-    mg_printf(conn, "HTTP/1.1 200 OK\r\n"
-              "Content-Type: text/plain\r\n\r\n"
-              "%s", request_info->uri);
-    return "";  // Mark as processed
+    const char *content = "hello from mongoose!";
+    mg_printf(conn,
+              "HTTP/1.1 200 OK\r\n"
+              "Content-Type: text/plain\r\n"
+              "Content-Length: %d\r\n"        // Always set Content-Length
+              "\r\n"
+              "%s",
+              strlen(content),
+              content);
+    // Mark as processed
+    return "";
   } else {
     return NULL;
   }
