4dff36ffde04a6c68bfcd7be13aa63c180ae58d3
Sergey Lyubka <valenok@gmail.com>
2012-09-22 13:36:24 +0100

    removed unused struct timespec

--- mongoose.c
+++ mongoose.c
@@ -141,11 +141,6 @@ typedef struct {HANDLE signal, broadcast;} pthread_cond_t;
 typedef DWORD pthread_t;
 #define pid_t HANDLE // MINGW typedefs pid_t to int. Using #define here.
 
-struct timespec {
-  long tv_nsec;
-  long tv_sec;
-};
-
 static int pthread_mutex_lock(pthread_mutex_t *);
 static int pthread_mutex_unlock(pthread_mutex_t *);
 static FILE *mg_fopen(const char *path, const char *mode);
