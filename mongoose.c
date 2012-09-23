2118de37c3c54d228d74878f2e15dabe103feab4
Sergey Lyubka <valenok@gmail.com>
2012-09-23 15:02:18 +0100

    3.3 -> 3.4

--- mongoose.c
+++ mongoose.c
@@ -218,7 +218,7 @@ typedef int SOCKET;
 
 #include "mongoose.h"
 
-#define MONGOOSE_VERSION "3.3"
+#define MONGOOSE_VERSION "3.4"
 #define PASSWORDS_FILE_NAME ".htpasswd"
 #define CGI_ENVIRONMENT_SIZE 4096
 #define MAX_CGI_ENVIR_VARS 64
