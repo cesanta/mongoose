88da48553860bcff88cd8c00f6b5dd7991016e6e
supermartian <supermartian@gmail.com>
2012-12-24 10:10:33 +0800

    Restore default signal dispositions for spawned processes.

--- mongoose.c
+++ mongoose.c
@@ -1392,6 +1392,11 @@ static pid_t spawn_process(struct mg_connection *conn, const char *prog,
 
       interp = conn->ctx->config[CGI_INTERPRETER];
       if (interp == NULL) {
+        // restore the signals for child processes
+        (void) signal(SIGCHLD, SIG_DFL);
+        (void) signal(SIGHUP, SIG_DFL);
+        (void) signal(SIGPIPE, SIG_DFL);
+
         (void) execle(prog, prog, NULL, envp);
         cry(conn, "%s: execle(%s): %s", __func__, prog, strerror(ERRNO));
       } else {
