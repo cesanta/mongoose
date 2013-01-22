4b29689a42cfd79bccfc67017f782c08a421b6e8
Sergey Lyubka <valenok@gmail.com>
2013-01-22 12:26:56 +0000

    Binding to 127.0.0.1 to prevent popup windows on mac

--- test/test.pl
+++ test/test.pl
@@ -158,8 +158,9 @@ if (scalar(@ARGV) > 0 and $ARGV[0] eq 'unit') {
 
 # Make sure we load config file if no options are given.
 # Command line options override config files settings
-write_file($config, "access_log_file access.log\nlistening_ports 12345\n");
-spawn("$exe -p $port");
+write_file($config, "access_log_file access.log\n" .
+           "listening_ports 127.0.0.1:12345\n");
+spawn("$exe -p 127.0.0.1:$port");
 o("GET /test/hello.txt HTTP/1.0\n\n", 'HTTP/1.1 200 OK', 'Loading config file');
 unlink $config;
 kill_spawned_child();
