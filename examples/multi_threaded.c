f7a705c40a104d7e4455f679095d1f4d059b020a
Sergey Lyubka <valenok@gmail.com>
2013-12-26 10:13:04 +0000

    Added multi-threaded example

---
+++ examples/multi_threaded.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "mongoose.h"
+
+static int request_handler(struct mg_connection *conn) {
+  mg_printf(conn, "HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\n\r\n"
+            "This is a reply from server instance # %s",
+            (char *) conn->server_param);
+  return 1;
+}
+
+static void *serve(void *server) {
+  for (;;) mg_poll_server((struct mg_server *) server, 1000);
+  return NULL;
+}
+
+int main(void) {
+  struct mg_server *server1, *server2;
+
+  server1 = mg_create_server("1");
+  server2 = mg_create_server("2");
+
+  mg_add_uri_handler(server1, "/", request_handler);
+  mg_add_uri_handler(server2, "/", request_handler);
+
+  // Make both server1 and server2 listen on the same socket
+  mg_set_option(server1, "listening_port", "8080");
+  mg_set_listening_socket(server2, mg_get_listening_socket(server1));
+
+  // server1 goes to separate thread, server 2 runs in main thread
+  mg_start_thread(serve, server1);
+  serve(server2);
+
+  return 0;
+}
