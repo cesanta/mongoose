9a99712ff327e9e8213b42751fbb8f5aa9c9a721
Boris Lytochkin <lytboris@gmail.com>
2014-07-22 11:03:55 +0400

    * fix build with only MONGOOSE_NO_DIRECTORY_LISTING defined * remove compiler warnings on redefined MONGOOSE_NO_*

--- mongoose.h
+++ mongoose.h
@@ -23,17 +23,37 @@
 #define MONGOOSE_VERSION "5.4"
 
 #ifdef MONGOOSE_NO_SOCKETPAIR
+#ifndef MONGOOSE_NO_CGI
 #define MONGOOSE_NO_CGI
+#endif
 #endif
 
 #ifdef MONGOOSE_NO_FILESYSTEM
+#ifndef MONGOOSE_NO_AUTH
 #define MONGOOSE_NO_AUTH
+#endif
+#ifndef MONGOOSE_NO_CGI
 #define MONGOOSE_NO_CGI
+#endif
+#ifndef MONGOOSE_NO_DAV
 #define MONGOOSE_NO_DAV
+#endif
+#ifndef MONGOOSE_NO_DIRECTORY_LISTING
 #define MONGOOSE_NO_DIRECTORY_LISTING
+#endif
+#ifndef MONGOOSE_NO_LOGGING
 #define MONGOOSE_NO_LOGGING
+#endif
+#ifndef MONGOOSE_NO_SSI
 #define MONGOOSE_NO_SSI
-#define MONGOOSE_NO_DL
+#endif
+#endif
+
+// DAV requires MONGOOSE_NO_DIRECTORY_LISTING
+#ifndef MONGOOSE_NO_DAV
+#ifdef MONGOOSE_NO_DIRECTORY_LISTING
+#undef MONGOOSE_NO_DIRECTORY_LISTING
+#endif
 #endif
 
 #include <stdio.h>      // required for FILE
