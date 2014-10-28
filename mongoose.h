632b3a38a99bb7b9f5ceca80132bacb72b942231
Sergey Lyubka <valenok@gmail.com>
2014-10-28 11:43:51 +0000

    5.5 -> 5.6

--- mongoose.h
+++ mongoose.h
@@ -20,7 +20,7 @@
 #ifndef MONGOOSE_HEADER_INCLUDED
 #define  MONGOOSE_HEADER_INCLUDED
 
-#define MONGOOSE_VERSION "5.5"
+#define MONGOOSE_VERSION "5.6"
 
 #include <stdio.h>      // required for FILE
 #include <stddef.h>     // required for size_t
