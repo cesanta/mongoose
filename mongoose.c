6668c456ac88c04a10b807944b5a91652d8d1c5a
Robert Di Paolo <rob.dipaolo@pebble.tv>
2015-03-18 09:56:01 +0000

    Correctly handle _CRT_SECURE_NO_WARNINGS already being set in project settings.

--- mongoose.c
+++ mongoose.c
@@ -44,7 +44,9 @@
 #undef _UNICODE                 // Use multibyte encoding on Windows
 #define _MBCS                   // Use multibyte encoding on Windows
 #define _INTEGRAL_MAX_BITS 64   // Enable _stati64() on Windows
+#ifndef _CRT_SECURE_NO_WARNINGS
 #define _CRT_SECURE_NO_WARNINGS // Disable deprecation warning in VS2005+
+#endif
 #undef WIN32_LEAN_AND_MEAN      // Let windows.h always include winsock2.h
 #ifdef __Linux__
 #define _XOPEN_SOURCE 600       // For flockfile() on Linux
