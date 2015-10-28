1a6bc7c51452162e5de00bc9fd59dafe831a7caa
Deomid Ryabkov <rojer@cesanta.com>
2015-10-28 23:49:08 +0000

    Fix mbuf_resize(0)
        PUBLISHED_FROM=7cab7543db990f7f67b53edca4f65ba45b219711

--- mongoose.c
+++ mongoose.c
@@ -143,11 +143,14 @@ void mbuf_free(struct mbuf *mbuf) {
 }
 
 void mbuf_resize(struct mbuf *a, size_t new_size) {
-  char *p;
-  if ((new_size > a->size || (new_size < a->size && new_size >= a->len)) &&
-      (p = (char *) MBUF_REALLOC(a->buf, new_size)) != NULL) {
-    a->size = new_size;
-    a->buf = p;
+  if (new_size > a->size || (new_size < a->size && new_size >= a->len)) {
+    a->buf = (char *) MBUF_REALLOC(a->buf, new_size);
+    /*
+     * In case realloc fails, there's not much we can do except set size to 0.
+     * Note that NULL is a valid return value from realloc when size == 0, but
+     * that is covered too.
+     */
+    a->size = (a->buf != NULL ? new_size : 0);
   }
 }
 
