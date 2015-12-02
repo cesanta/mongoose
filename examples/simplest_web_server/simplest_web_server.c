f5ad19d76baa9c59cd553de47bbd910dc28c9137
Sergey Lyubka <lsm@cesanta.com>
2015-12-02 16:53:04 +0000

    Fix g++ build for mongoose example
        PUBLISHED_FROM=c581f33e26819d05c9b8484c9dc10c2a3615a34a

--- examples/simplest_web_server/simplest_web_server.c
+++ examples/simplest_web_server/simplest_web_server.c
@@ -8,7 +8,7 @@ static struct mg_serve_http_opts s_http_server_opts;
 
 static void ev_handler(struct mg_connection *nc, int ev, void *p) {
   if (ev == MG_EV_HTTP_REQUEST) {
-    mg_serve_http(nc, p, s_http_server_opts);
+    mg_serve_http(nc, (struct http_message *) p, s_http_server_opts);
   }
 }
 
