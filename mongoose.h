19a28b3c818483cba42c662f5ca5235eb12cebe6
Sergey Lyubka <lsm@cesanta.com>
2016-05-03 15:13:52 +0300

    Update cs_md5 description according to reality
    PUBLISHED_FROM=b9c776bac4c8f2456c71b33641601fb38b30347e

--- mongoose.h
+++ mongoose.h
@@ -966,11 +966,12 @@ void MD5_Update(MD5_CTX *c, const unsigned char *data, size_t len);
 void MD5_Final(unsigned char *md, MD5_CTX *c);
 
 /*
- * Return stringified MD5 hash for NULL terminated list of strings.
+ * Return stringified MD5 hash for NULL terminated list of pointer/length pairs.
+ * A length should be specified as size_t variable.
  * Example:
  *
  *    char buf[33];
- *    cs_md5(buf, "foo", "bar", NULL);
+ *    cs_md5(buf, "foo", (size_t) 3, "bar", (size_t) 3, NULL);
  */
 char *cs_md5(char buf[33], ...);
 
