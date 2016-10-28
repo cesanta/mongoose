97cff849e5241937cb33ff3a07e6284720b49d88
Dmitry Frank <dmitry.frank@cesanta.com>
2016-10-28 11:57:21 +0100

    Make mongoose compile for nRF51
    PUBLISHED_FROM=6af3ed56802d2619f673c36059370440a0c06397

--- mongoose.c
+++ mongoose.c
@@ -10243,7 +10243,7 @@ int gettimeofday(struct timeval *tp, void *tzp) {
  * All rights reserved
  */
 
-#if CS_PLATFORM == CS_P_NRF52 && defined(__ARMCC_VERSION)
+#if (CS_PLATFORM == CS_P_NRF51 || CS_PLATFORM == CS_P_NRF52) && defined(__ARMCC_VERSION)
 int gettimeofday(struct timeval *tp, void *tzp) {
   /* TODO */
   tp->tv_sec = 0;
