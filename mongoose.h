393ddc6ba55434d392b39676e9e6568f457e6aef
Alexander Alashkin <alexander.alashkin@cesanta.com>
2017-01-10 20:20:35 +0200

    c_hello/stm32-cube, part 2
    PUBLISHED_FROM=fe7230811b7f12286c5ac6de1ef8fb068cd61145

--- mongoose.h
+++ mongoose.h
@@ -1545,11 +1545,12 @@ char* inet_ntoa(struct in_addr in);
 
 typedef int sock_t;
 
-struct sockaddr {
-};
-
 struct in_addr{
   int s_addr;
+};
+
+struct sockaddr {
+  int sa_family;
 };
 
 struct sockaddr_in {
@@ -1572,6 +1573,28 @@ struct sockaddr_in {
 #define htons(x) (x)
 #define ntohs(x) (x)
 #define ntohl(x) (x)
+
+#define F_GETFL -1
+#define F_SETFL -1
+#define O_NONBLOCK -1
+#define fcntl(x1, x2, x3) (-1)
+#define socket(x1, x2, x3) (-1)
+#define connect(x1, x2, x3) (-1)
+#define setsockopt(x1, x2, x3, x4, x5) (-1)
+#define closesocket(x)
+typedef int socklen_t;
+#define accept(x1, x2, x3) (-1)
+#define SOMAXCONN -1
+#define listen(x1, x2) (-1)
+#define bind(x1, x2, x3) (-1)
+#define getsockname(x1, x2, x3) (-1)
+#define sendto(x1, x2, x3, x4, x5, x6) (-1)
+#define send(x1, x2, x3, x4) (-1)
+#define recvfrom(x1, x2, x3, x4, x5, x6) (-1)
+#define getsockopt(x1, x2, x3, x4, x5) (-1)
+#define getpeername(x1, x2, x3) (-1)
+#define recv(s, b, l, f) (-1)
+#define select(x1, x2, x3, x4, x5) (0)
 
 const char *inet_ntop(int af, const void *src, char *dst, int size);
 
