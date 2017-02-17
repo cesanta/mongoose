cd1d5e23ebf11245a8f9d36a2a6d88eb2ecf53b2
Deomid Ryabkov <rojer@cesanta.com>
2017-02-17 22:47:49 +0800

    Fix ESP8266_RTOS example; add mos flashing
    PUBLISHED_FROM=d87814d40f0acdc7a96fbd132c2157ccb499c7c6

--- examples/ESP8266_RTOS/build.sh
+++ examples/ESP8266_RTOS/build.sh
@@ -6,5 +6,6 @@ docker run \
     export SDK_PATH=/opt/Espressif/ESP8266_RTOS_SDK;
     export BIN_PATH=./bin;
     cd /src/examples/ESP8266_RTOS &&
-    mkdir -p ./bin && make clean &&
-    make BOOT=none APP=0 SPI_SPEED=40 SPI_MODE=qio SPI_SIZE_MAP=0'
+    make clean &&
+    make BOOT=none APP=0 SPI_SPEED=40 SPI_MODE=dio SPI_SIZE_MAP=0 &&
+    make fw.zip'
