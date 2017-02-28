874b5bfdbff2a4f47aa2629211ef9360b32d04fd
Yiming Sun <bearsugar@foxmail.com>
2017-02-28 12:33:56 +0200

    Fix comment in mongoose.h
    PUBLISHED_FROM=35e8f18aa611b45c271d0178d9197291bfa9726d

--- docs/c-api/http_server.h/mg_register_http_endpoint.md
+++ docs/c-api/http_server.h/mg_register_http_endpoint.md
@@ -20,7 +20,7 @@ static void handle_hello1(struct mg_connection *nc, int ev, void *ev_data) {
  nc->flags |= MG_F_SEND_AND_CLOSE;
 }
 
-static void handle_hello1(struct mg_connection *nc, int ev, void *ev_data) {
+static void handle_hello2(struct mg_connection *nc, int ev, void *ev_data) {
  (void) ev; (void) ev_data;
   mg_printf(nc, "HTTP/1.0 200 OK\r\n\r\n[I am Hello2]");
  nc->flags |= MG_F_SEND_AND_CLOSE;
--- mongoose.h
+++ mongoose.h
@@ -4742,7 +4742,7 @@ void mg_file_upload_handler(struct mg_connection *nc, int ev, void *ev_data,
  *  nc->flags |= MG_F_SEND_AND_CLOSE;
  * }
  *
- * static void handle_hello1(struct mg_connection *nc, int ev, void *ev_data) {
+ * static void handle_hello2(struct mg_connection *nc, int ev, void *ev_data) {
  *  (void) ev; (void) ev_data;
  *   mg_printf(nc, "HTTP/1.0 200 OK\r\n\r\n[I am Hello2]");
  *  nc->flags |= MG_F_SEND_AND_CLOSE;
