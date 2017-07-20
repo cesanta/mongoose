fea648496cf134a1aedddd81ee35bbc05e39853a
Sergey Lyubka <sergey.lyubka@cesanta.com>
2017-07-20 09:59:18 +0200

    Fix https://github.com/cesanta/mongoose-os/issues/318
    PUBLISHED_FROM=2fc7b59b2edd0f473acd22067439471778acef8e

--- mongoose.c
+++ mongoose.c
@@ -7919,6 +7919,9 @@ struct mg_connection *mg_connect_http_base(
     goto out;
   }
 
+  /* If query is present, do not strip it. Pass to the caller. */
+  if (query.len > 0) path->len += query.len + 1;
+
   if (scheme.len == 0 || mg_vcmp(&scheme, scheme1) == 0 ||
       (scheme2 != NULL && mg_vcmp(&scheme, scheme2) == 0)) {
     use_ssl = 0;
