048480bc7d46e0f8d40f1c3d7a110a931ad00a28
Michael Balzer <balzer@expeedo.de>
2018-04-28 23:24:36 +0200

    Enable using getaddrinfo() in mg_parse_address()

--- mongoose.c
+++ mongoose.c
@@ -2621,9 +2621,11 @@ MG_INTERNAL int mg_parse_address(const char *str, union socket_address *sa,
        * try to resolve it using `gethostbyname` and do not try
        * to resolve it via DNS server if gethostbyname has failed too
        */
+#if !(MG_ENABLE_SYNC_RESOLVER && MG_ENABLE_GETADDRINFO)
       if (mg_ncasecmp(host, "localhost", 9) != 0) {
         return 0;
       }
+#endif
 
 #if MG_ENABLE_SYNC_RESOLVER
       if (!mg_resolve2(host, &sa->sin.sin_addr)) {
