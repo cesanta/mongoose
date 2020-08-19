a3691602960e0a479565b439645e6b883a11a273
Deomid Ryabkov <rojer@rojer.me>
2020-08-19 21:48:45 +0100

    Add missing range check in mg_get_http_header()
    h/t BushraAloraini
    
    Fixes https://github.com/cesanta/mongoose/issues/1135

--- mongoose.c
+++ mongoose.c
@@ -6160,7 +6160,7 @@ int mg_parse_http(const char *s, int n, struct http_message *hm, int is_req) {
 struct mg_str *mg_get_http_header(struct http_message *hm, const char *name) {
   size_t i, len = strlen(name);
 
-  for (i = 0; hm->header_names[i].len > 0; i++) {
+  for (i = 0; i < MG_MAX_HTTP_HEADERS && hm->header_names[i].len > 0; i++) {
     struct mg_str *h = &hm->header_names[i], *v = &hm->header_values[i];
     if (h->p != NULL && h->len == len && !mg_ncasecmp(h->p, name, len))
       return v;
