51e3a1304b2077512b1dbd373075eb522f7b9022
cpq <valenok@gmail.com>
2021-02-13 10:45:27 +0000

    Fix TLS build

--- examples/http-client/main.c
+++ examples/http-client/main.c
@@ -22,8 +22,7 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
 
     // If s_url is https://, tell client connection to use TLS
     if (mg_url_is_ssl(s_url)) {
-      // struct mg_tls_opts opts = {.ca = "ca.pem", .srvname = host};
-      struct mg_tls_opts opts = {.ca = "ca.pem", .srvname = mg_str("x")};
+      struct mg_tls_opts opts = {.ca = "ca.pem", .srvname = host};
       mg_tls_init(c, &opts);
     }
 
