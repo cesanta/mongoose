8e5efd4804ce5b89e0b268fc91e99b6b8c1cd78a
Sergey Lyubka <valenok@gmail.com>
2021-07-23 12:17:05 +0100

    7.3 -> 7.4

--- mongoose.h
+++ mongoose.h
@@ -18,7 +18,7 @@
 #ifndef MONGOOSE_H
 #define MONGOOSE_H
 
-#define MG_VERSION "7.3"
+#define MG_VERSION "7.4"
 
 #ifdef __cplusplus
 extern "C" {
--- src/version.h
+++ src/version.h
@@ -2,7 +2,7 @@
 #ifndef MONGOOSE_H
 #define MONGOOSE_H
 
-#define MG_VERSION "7.3"
+#define MG_VERSION "7.4"
 
 #ifdef __cplusplus
 extern "C" {
