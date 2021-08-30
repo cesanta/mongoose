28a56370d45a674e44f151988a197be9e75c2695
Sergey Lyubka <valenok@gmail.com>
2021-08-30 11:39:19 +0100

    Fix zeromem() call in mg_iobuf_del()

--- src/iobuf.c
+++ src/iobuf.c
@@ -62,7 +62,7 @@ size_t mg_iobuf_del(struct mg_iobuf *io, size_t ofs, size_t len) {
   if (ofs > io->len) ofs = io->len;
   if (ofs + len > io->len) len = io->len - ofs;
   memmove(io->buf + ofs, io->buf + ofs + len, io->len - ofs - len);
-  zeromem(io->buf + ofs + io->len - len, len);
+  zeromem(io->buf + io->len - len, len);
   io->len -= len;
   return len;
 }
