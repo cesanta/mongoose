37d45b3fbbe20f8d1fa78bab6aebe06f4002ff5d
James Hilliard <james.hilliard1@gmail.com>
2021-10-13 15:02:58 -0600

    sntp: Don't set Leap Indicator in client messages.
    Per RFC4330:
    This field is significant only in server messages
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>

--- src/sntp.c
+++ src/sntp.c
@@ -52,7 +52,7 @@ void mg_sntp_send(struct mg_connection *c, unsigned long utc) {
   } else if (utc > s_sntmp_next) {
     uint8_t buf[48] = {0};
     s_sntmp_next = utc + SNTP_INTERVAL_SEC;
-    buf[0] = (3 << 6) | (4 << 3) | 3;
+    buf[0] = (0 << 6) | (4 << 3) | 3;
     mg_send(c, buf, sizeof(buf));
     LOG(LL_DEBUG,
         ("%p request sent, ct %lu, next at %lu", c->fd, utc, s_sntmp_next));
