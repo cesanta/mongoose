ce6164ca206001e6e6d404a3a9b2adbbf656026a
Sergey Lyubka <valenok@gmail.com>
2021-12-22 18:04:16 +0000

    Fail stat() if S_ISDIR is not defined

--- src/fs_posix.c
+++ src/fs_posix.c
@@ -2,7 +2,11 @@
 
 #if MG_ENABLE_FILE
 static int p_stat(const char *path, size_t *size, time_t *mtime) {
-#ifdef _WIN32
+#if !defined(S_ISDIR)
+  LOG(LL_ERROR, ("stat() API is not supported. %p %p %p", path, size, mtime));
+  return 0;
+#else
+#if defined(_WIN32)
   struct _stati64 st;
   wchar_t tmp[PATH_MAX];
   MultiByteToWideChar(CP_UTF8, 0, path, -1, tmp, sizeof(tmp) / sizeof(tmp[0]));
@@ -14,6 +18,7 @@ static int p_stat(const char *path, size_t *size, time_t *mtime) {
   if (size) *size = (size_t) st.st_size;
   if (mtime) *mtime = st.st_mtime;
   return MG_FS_READ | MG_FS_WRITE | (S_ISDIR(st.st_mode) ? MG_FS_DIR : 0);
+#endif
 }
 
 #ifdef _WIN32
