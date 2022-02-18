412f5cf3e04a67fea0c7ad057ad8f7c71ca9c843
Alex Alashkin <alex.gremlin@outlook.com>
2022-02-18 10:07:10 +0200

    Add RTX-RTOS support

--- src/arch.h
+++ src/arch.h
@@ -8,6 +8,7 @@
 #define MG_ARCH_FREERTOS_TCP 5
 #define MG_ARCH_FREERTOS_LWIP 6
 #define MG_ARCH_AZURERTOS 7
+#define MG_ARCH_RTX_LWIP 8
 
 #if !defined(MG_ARCH)
 #if defined(__unix__) || defined(__APPLE__)
