1893b06c13fef1a7f7fd735690f641ec5aacd861
Sergey Lyubka <valenok@gmail.com>
2022-02-23 13:31:00 +0000

    Fix #1472 - calm static analyzer

--- mongoose.c
+++ mongoose.c
@@ -925,9 +925,9 @@ static size_t p_seek(void *fp, size_t offset) {
 #if (defined(_FILE_OFFSET_BITS) && _FILE_OFFSET_BITS == 64) ||  \
     (defined(_POSIX_C_SOURCE) && _POSIX_C_SOURCE >= 200112L) || \
     (defined(_XOPEN_SOURCE) && _XOPEN_SOURCE >= 600)
-  fseeko((FILE *) fp, (off_t) offset, SEEK_SET);
+  if (fseeko((FILE *) fp, (off_t) offset, SEEK_SET) != 0) (void) 0;
 #else
-  fseek((FILE *) fp, (long) offset, SEEK_SET);
+  if (fseek((FILE *) fp, (long) offset, SEEK_SET) != 0) (void) 0;
 #endif
   return (size_t) ftell((FILE *) fp);
 }
