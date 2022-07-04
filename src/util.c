735b7bece6ce3686323ee72a582e1cd95c6a9391
Sergey Lyubka <valenok@gmail.com>
2022-07-04 11:43:52 +0100

    Squash more warns

--- src/util.c
+++ src/util.c
@@ -26,7 +26,7 @@ char *mg_random_str(char *buf, size_t len) {
   mg_random(buf, len);
   for (i = 0; i < len; i++) {
     uint8_t c = ((uint8_t *) buf)[i] % 62;
-    buf[i] = i == len - 1 ? '\0'                   // 0-terminate last byte
+    buf[i] = i == len - 1 ? (char) '\0'            // 0-terminate last byte
              : c < 26     ? (char) ('a' + c)       // lowercase
              : c < 52     ? (char) ('A' + c - 26)  // uppercase
                           : (char) ('0' + c - 52);     // numeric
