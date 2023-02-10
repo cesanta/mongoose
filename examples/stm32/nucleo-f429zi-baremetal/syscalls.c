1cb99ea2fca9b6b515c41d32234e149bbe7b3e75
Sergio R. Caprile <scaprile@cesanta.com>
2023-02-10 15:53:55 -0300

    Standardize F4 baremetal

--- examples/stm32/nucleo-f429zi-baremetal/syscalls.c
+++ examples/stm32/nucleo-f429zi-baremetal/syscalls.c
@@ -53,7 +53,7 @@ int _getpid(void) {
 
 int _write(int fd, char *ptr, int len) {
   (void) fd, (void) ptr, (void) len;
-  if (fd == 1) uart_write_buf(UART1, ptr, (size_t) len);
+  if (fd == 1) uart_write_buf(UART_DEBUG, ptr, (size_t) len);
   return -1;
 }
 
