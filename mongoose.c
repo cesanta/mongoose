20c5da6e555c1f03d171785e65df94ee2f245d5f
cpq <valenok@gmail.com>
2023-02-28 17:55:25 +0000

    Fix build - NULL pointer check

--- mongoose.c
+++ mongoose.c
@@ -4930,6 +4930,7 @@ const char *mg_strstr(const struct mg_str haystack,
                       const struct mg_str needle) {
   size_t i;
   if (needle.len > haystack.len) return NULL;
+  if (needle.len == 0) return haystack.ptr;
   for (i = 0; i <= haystack.len - needle.len; i++) {
     if (memcmp(haystack.ptr + i, needle.ptr, needle.len) == 0) {
       return haystack.ptr + i;
