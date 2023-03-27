a819513a925ff6fed22f9e7e632b55deeb281c69
Sergio R. Caprile <scaprile@cesanta.com>
2023-03-27 18:14:55 -0300

    Remove old example and its breadcrumbs

--- test/freertos-tcp/portable/NetworkInterface/STM32Fxx/stm32f2xx_hal_eth.h
+++
@@ -1,6 +0,0 @@
-/*
- * The Ethernet header files for STM32F2, STM32F4 and STM32F7 have been merged to
- * a single module that works for both parts: "stm32fxx_hal_eth"
- */
-
-#include "stm32fxx_hal_eth.h"
