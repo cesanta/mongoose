cca771e70e862053b54bf7a724c4b4926341e2c1
cpq <valenok@gmail.com>
2023-07-01 20:23:59 +0100

    Stricter check for test_dns_error()

--- test/unit_test.c
+++ test/unit_test.c
@@ -1920,8 +1920,10 @@ static void test_str(void) {
 }
 
 static void fn1(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
-  if (ev == MG_EV_ERROR)
+  if (ev == MG_EV_ERROR) {
+    ASSERT(* (void **) fn_data == NULL);
     *(char **) fn_data = mg_mprintf("%s", (char *) ev_data);
+  }
   (void) c;
 }
 
