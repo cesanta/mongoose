8d90322f3b479845a0c686963087833e2dc83f48
Sergio R. Caprile <scaprile@cesanta.com>
2023-07-18 17:21:35 -0300

    static clue

--- extra/cmsis-pack/Boards/ST/NUCLEO-F746ZG/nucleo-f746zg-keil-baremetal/main.c
+++ extra/cmsis-pack/Boards/ST/NUCLEO-F746ZG/nucleo-f746zg-keil-baremetal/main.c
@@ -43,10 +43,12 @@ int main(void) {
   mg_log_set(MG_LL_DEBUG);  // Set log level
 
   // Initialise Mongoose network stack
-  // Specify MAC address, and IP/mask/GW in network byte order for static
-  // IP configuration. If IP/mask/GW are unset, DHCP is going to be used
   struct mg_tcpip_driver_stm32_data driver_data = {.mdc_cr = 4};
   struct mg_tcpip_if mif = {.mac = GENERATE_LOCALLY_ADMINISTERED_MAC(),
+                            // Uncomment below for static configuration:
+                            // .ip = mg_htonl(MG_U32(192, 168, 0, 223)),
+                            // .mask = mg_htonl(MG_U32(255, 255, 255, 0)),
+                            // .gw = mg_htonl(MG_U32(192, 168, 0, 1)),
                             .driver = &mg_tcpip_driver_stm32,
                             .driver_data = &driver_data};
   mg_tcpip_init(&mgr, &mif);
