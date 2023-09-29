a628a05efb8a32e4d696960121ed13f274a2a907
cpq <valenok@gmail.com>
2023-09-29 13:09:29 +0100

    Restore pub. domain notice

--- src/md5.c
+++ src/md5.c
@@ -1,6 +1,21 @@
 #include "arch.h"
 #include "md5.h"
 
+//  This code implements the MD5 message-digest algorithm.
+//  The algorithm is due to Ron Rivest.  This code was
+//  written by Colin Plumb in 1993, no copyright is claimed.
+//  This code is in the public domain; do with it what you wish.
+//
+//  Equivalent code is available from RSA Data Security, Inc.
+//  This code has been tested against that, and is equivalent,
+//  except that you don't need to include two pages of legalese
+//  with every copy.
+//
+//  To compute the message digest of a chunk of bytes, declare an
+//  MD5Context structure, pass it to MD5Init, call MD5Update as
+//  needed on buffers full of bytes, and then call MD5Final, which
+//  will fill a supplied 16-byte array with the digest.
+
 #if defined(MG_ENABLE_MD5) && MG_ENABLE_MD5
 
 static void mg_byte_reverse(unsigned char *buf, unsigned longs) {
