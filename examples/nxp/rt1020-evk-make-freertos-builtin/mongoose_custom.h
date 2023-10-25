6dabbe237d2725eb2a0e94e158721899a191f495
Sergio R. Caprile <scaprile@cesanta.com>
2023-10-25 14:56:29 -0300

    unbreak RT1020 examples after removing IMX prefix

--- examples/nxp/rt1020-evk-make-freertos-builtin/mongoose_custom.h
+++ examples/nxp/rt1020-evk-make-freertos-builtin/mongoose_custom.h
@@ -5,7 +5,7 @@
 // See https://mongoose.ws/documentation/#build-options
 #define MG_ARCH MG_ARCH_FREERTOS
 #define MG_ENABLE_TCPIP 1
-#define MG_ENABLE_DRIVER_IMXRT1020 1
+#define MG_ENABLE_DRIVER_RT1020 1
 #define MG_IO_SIZE 256
 #define MG_ENABLE_CUSTOM_RANDOM 1
 #define MG_ENABLE_PACKED_FS 1
