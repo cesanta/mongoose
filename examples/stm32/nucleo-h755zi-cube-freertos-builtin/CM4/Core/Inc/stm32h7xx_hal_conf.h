80315af9443d6213dfab388e09e22ba4e758f322
Claudio Eterno <eterno.claudio@gmail.com>
2024-01-11 10:38:36 +0100

    some corrections

--- examples/stm32/nucleo-h755zi-cube-freertos-builtin/CM4/Core/Inc/stm32h7xx_hal_conf.h
+++ examples/stm32/nucleo-h755zi-cube-freertos-builtin/CM4/Core/Inc/stm32h7xx_hal_conf.h
@@ -236,7 +236,7 @@
   * @brief Uncomment the line below to expanse the "assert_param" macro in the
   *        HAL drivers code
   */
- #define USE_FULL_ASSERT    1U
+/* #define USE_FULL_ASSERT    1U */
 
 /* Includes ------------------------------------------------------------------*/
 /**
