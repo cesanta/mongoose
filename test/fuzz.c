89aaf1c30c20db939154121d4eb0320339b2f053
Sergio R. Caprile <scaprile@cesanta.com>
2024-04-19 18:28:13 -0300

    move Makefile to test/

--- test/fuzz.c
+++ test/fuzz.c
@@ -123,13 +123,12 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
 int main(int argc, char *argv[]) {
   int res = EXIT_FAILURE;
   if (argc > 1) {
-    size_t len = 0;
-    char *buf = mg_file_read(&mg_fs_posix, argv[1], &len);
-    if (buf != NULL) {
-      LLVMFuzzerTestOneInput((uint8_t *) buf, len);
+    struct mg_str data = mg_file_read(&mg_fs_posix, argv[1]);
+    if (data.buf != NULL) {
+      LLVMFuzzerTestOneInput((uint8_t *) data.buf, data.len);
       res = EXIT_SUCCESS;
     }
-    free(buf);
+    free(data.buf);
   }
   return res;
 }
