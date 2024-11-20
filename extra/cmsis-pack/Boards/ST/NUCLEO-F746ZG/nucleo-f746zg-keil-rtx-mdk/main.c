f365ea363d20417d5484f1a904c420db999695f0
Sergio R. Caprile <scaprile@cesanta.com>
2024-11-20 14:51:29 -0300

    update CMSIS-pack to 7.16

--- extra/cmsis-pack/Boards/ST/NUCLEO-F746ZG/nucleo-f746zg-keil-rtx-mdk/main.c
+++ extra/cmsis-pack/Boards/ST/NUCLEO-F746ZG/nucleo-f746zg-keil-rtx-mdk/main.c
@@ -10,13 +10,14 @@
 #define BLINK_PERIOD_MS 1000  // LED blinking period in millis
 
 
-void mg_random(void *buf, size_t len) {  // Use on-board RNG
+bool mg_random(void *buf, size_t len) {  // Use on-board RNG
   extern RNG_HandleTypeDef hrng;
   for (size_t n = 0; n < len; n += sizeof(uint32_t)) {
     uint32_t r;
     HAL_RNG_GenerateRandomNumber(&hrng, &r);
     memcpy((char *) buf + n, &r, n + sizeof(r) > len ? len - n : sizeof(r));
   }
+  return true;
 }
 
 static void server(const void *args) {
