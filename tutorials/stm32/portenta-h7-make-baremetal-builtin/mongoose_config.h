92d4b482705ff9754a826dbf2012a1dcfe4356bc
Sergio R. Caprile <scaprile@cesanta.com>
2025-04-17 18:58:49 -0300

    Add support for SDIO Add support for SDIO in CYW driver Add Portenta H7 example

---
+++ tutorials/stm32/portenta-h7-make-baremetal-builtin/mongoose_config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// See https://mongoose.ws/documentation/#build-options
+#define MG_ARCH MG_ARCH_NEWLIB
+#define MG_TLS MG_TLS_BUILTIN
+#define MG_OTA MG_OTA_STM32H7_DUAL_CORE
+
+#define MG_ENABLE_TCPIP 1
+#define MG_ENABLE_CUSTOM_MILLIS 1
+#define MG_ENABLE_CUSTOM_RANDOM 1
+#define MG_ENABLE_PACKED_FS 1 
+#define MG_ENABLE_DRIVER_CYW_SDIO 1
+#define MG_ENABLE_TCPIP_DRIVER_INIT 0
