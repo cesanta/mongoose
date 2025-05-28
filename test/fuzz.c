b197a674e19c794c328ce12a00a07a8dae65909b
Sergio R. Caprile <scaprile@cesanta.com>
2025-05-28 13:56:04 -0300

    fix memory leak in fuzz test itself

--- test/fuzz.c
+++ test/fuzz.c
@@ -146,6 +146,7 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     c->pfn(c, MG_EV_READ, NULL); // manually invoke protocol event handler
 
     free(pkt);
+    mg_mgr_free(&mgr);
   }
 
   return 0;
