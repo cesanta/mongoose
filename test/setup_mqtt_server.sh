587b074822e8bc64f71becc8b05853e31e774ff3
Sergio R. Caprile <scaprile@cesanta.com>
2025-07-24 17:13:06 -0300

    fix mingw[++] tests, add local MQTTS

--- test/setup_mqtt_server.sh
+++ test/setup_mqtt_server.sh
@@ -3,7 +3,7 @@
 sudo apt-get -y install mosquitto
 sudo cp test/mosquitto.conf /etc/mosquitto/conf.d/
 sudo cp test/certs/ca.crt /etc/mosquitto/certs/
-sudo cp test/certs/serverc.crt /etc/mosquitto/certs/
-sudo cp test/certs/serverc.key /etc/mosquitto/certs/
+sudo cp test/certs/server.crt /etc/mosquitto/certs/
+sudo cp test/certs/server.key /etc/mosquitto/certs/
 sudo chmod a+w /var/log/mosquitto/*
 sudo mosquitto -c /etc/mosquitto/mosquitto.conf &
