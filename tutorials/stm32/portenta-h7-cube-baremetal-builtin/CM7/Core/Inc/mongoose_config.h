8eaca9437e006890d0275728c6dc092f48fb937b
Sergio R. Caprile <scaprile@cesanta.com>
2025-11-27 17:08:00 -0300

    fix Portenta Cube after cyw.h updates

--- tutorials/stm32/portenta-h7-cube-baremetal-builtin/CM7/Core/Inc/mongoose_config.h
+++ tutorials/stm32/portenta-h7-cube-baremetal-builtin/CM7/Core/Inc/mongoose_config.h
@@ -3,34 +3,11 @@
 // See https://mongoose.ws/documentation/#build-options
 #define MG_ARCH MG_ARCH_ARMGCC
 #define MG_TLS MG_TLS_BUILTIN
-#define MG_OTA MG_OTA_STM32H7_DUAL_CORE
+//#define MG_OTA MG_OTA_STM32H7_DUAL_CORE
 #define MG_IRAM __attribute__((section(".RamFunc")))
 
 #define MG_ENABLE_TCPIP 1
 #define MG_ENABLE_CUSTOM_MILLIS 1
 #define MG_ENABLE_CUSTOM_RANDOM 1
 #define MG_ENABLE_DRIVER_CYW_SDIO 1
-#define MG_ENABLE_PACKED_FS 1
-
-// #define MG_DRIVER_MDC_CR 4   // RMII MDC clock divider, from 0 to 5
-// #define MG_TCPIP_PHY_ADDR 0  // PHY address
-
-// For static IP configuration, define MG_TCPIP_{IP,MASK,GW}
-// By default, those are set to zero, meaning that DHCP is used
-//
-// #define MG_TCPIP_IP MG_IPV4(192, 168, 0, 10)     // IP
-// #define MG_TCPIP_GW MG_IPV4(192, 168, 0, 1)      // Gateway
-// #define MG_TCPIP_MASK MG_IPV4(255, 255, 255, 0)  // Netmask
-
-// Construct MAC address from the MCU unique ID. It is defined in the
-// ST CMSIS header as UID_BASE
-#define MGUID ((uint32_t *) 0x1ff1e800)  // Unique 96-bit chip ID
-#define MG_SET_MAC_ADDRESS(mac)      \
-  do {                               \
-    mac[0] = 2;                      \
-    mac[1] = MGUID[0] & 255;         \
-    mac[2] = (MGUID[0] >> 10) & 255; \
-    mac[3] = (MGUID[0] >> 19) & 255; \
-    mac[4] = MGUID[1] & 255;         \
-    mac[5] = MGUID[2] & 255;         \
-  } while (0)
+#define MG_ENABLE_TCPIP_DRIVER_INIT 0
--- tutorials/stm32/portenta-h7-make-baremetal-builtin/mongoose_config.h
+++ tutorials/stm32/portenta-h7-make-baremetal-builtin/mongoose_config.h
@@ -3,7 +3,7 @@
 // See https://mongoose.ws/documentation/#build-options
 #define MG_ARCH MG_ARCH_ARMGCC
 #define MG_TLS MG_TLS_BUILTIN
-#define MG_OTA MG_OTA_STM32H7_DUAL_CORE
+//#define MG_OTA MG_OTA_STM32H7_DUAL_CORE
 
 #define MG_ENABLE_TCPIP 1
 #define MG_ENABLE_CUSTOM_MILLIS 1
