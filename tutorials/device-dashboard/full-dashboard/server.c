8e3c8bf61f6c518ea3d46536d1cd2587ad09a5b3
cpq <sergey.lyubka@cesanta.com>
2026-04-19 08:58:14 +0100

    Add graph and control panel

--- tutorials/device-dashboard/full-dashboard/server.c
+++ tutorials/device-dashboard/full-dashboard/server.c
@@ -2,12 +2,47 @@
 // SPDX-License-Identifier: GPL-2.0-only or commercial
 
 #include "mongoose.h"
+#include "math.h"
 
 #if MG_ARCH == MG_ARCH_UNIX || MG_ARCH == MG_ARCH_WIN32
 #define HTTP_ADDR "http://0.0.0.0:8000"
 #else
 #define HTTP_ADDR "http://0.0.0.0:80"
 #endif
+
+#define NUM_POINTS 256   // How many data points to send
+// #define INTERVAL_MS 200  // How often, in milliseconds
+
+struct point {
+  uint16_t x;   // X coordinate of the data point, usually this is time
+  uint16_t y1;  // We can plot multiple values per graph,
+  uint16_t y2;  // let's plot 2 values: y1 and y2.
+};
+
+// This function prints a JSON array of plot points
+size_t print_points(void (*fn)(char, void *), void *arg, va_list *ap) {
+  size_t i, n = 0;
+  const char *comma = "";
+  (void) ap;
+  n += mg_xprintf(fn, arg, "[");
+  for (i = 0; i < NUM_POINTS; i++) {
+    struct point p;
+    p.x = (uint16_t) i;
+    // rand() - 0.5 + sin(x * 0.00002) * 40 + sin(x * 0.001) * 5 + sin(x * 0.1)
+    // * 2;
+    p.y1 = 100 + 50 * sin(i * 0.1) + rand() % 21;
+    p.y2 = 70 + 30 * sin(i * 0.03) + rand() % 17;
+    n += mg_xprintf(fn, arg, "%s[%hu,%hu,%hu]", comma, p.x, p.y1, p.y2);
+    comma = ",";
+  }
+  n += mg_xprintf(fn, arg, "]");
+  return n;
+}
+
+static void get_points(union mg_val *val) {
+  val->fn = (void (*)(void)) print_points;
+}
+
 
 static bool s_led;
 
@@ -22,6 +57,7 @@ static void set_led(const union mg_val *val) {
 // Modify this. This represents device's state to the dashboard
 static struct mg_field fields[] = {
     {"led", MG_VAL_BOOL, get_led, set_led},
+    {"points", MG_VAL_FN, get_points, NULL},
     {NULL, 0, 0, 0},
 };
 
--- tutorials/device-dashboard/full-dashboard/web_root/main.js
+++ tutorials/device-dashboard/full-dashboard/web_root/main.js
@@ -31,4 +31,43 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const initialId = window.location.hash.slice(1) || "overview";
   activateTab(initialId);
+
+
+  const el = document.getElementById("chart");
+  const rect = el.getBoundingClientRect();
+
+  const options = {
+    width: rect.width,
+    height: rect.height,
+    tzDate: (ts) => uPlot.tzDate(new Date(ts * 1e3), "Europe/London"),
+    plugins: [],
+    legend: { show: false },
+    axes: [
+      { values: "{MMM} {DD}", size: 25 },
+      { size: 50, xvalues: (u, vals, space) => vals.map(v => (+v / 1000).toFixed(1) + 'k') },
+    ],
+    series: [
+      {},
+      {stroke: 'rgba(5, 141, 199, 1)', fill: 'rgba(5, 141, 199, 0.1)'},
+      {stroke: 'rgba(237, 126, 23, 1)', fill: 'rgba(237, 126, 23, 0.1)'},
+    ],
+  };
+
+  // Create chart instance
+  const chart = new uPlot(options, [], el);
+
+  function updateGraphPoints(args) {
+    const data = [
+      args.points.map(x => x[0]),
+      args.points.map(x => x[1]),
+      args.points.map(x => x[2]),
+    ];
+    //console.log(123, args.points);
+    chart.setData(data);
+  };
+
+  // Intercept change notifications
+  Dashboard.on('change', args => updateGraphPoints(args));
+  Dashboard.call('get', 'points').then(resp => updateGraphPoints(resp));
+
 });
