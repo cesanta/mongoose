7ab79669ef6c31767da5bf81399f0a6f8ef8828c
cpq <sergey.lyubka@cesanta.com>
2026-05-12 13:29:04 +0100

    Update downloads

--- resources/downloads/init.sh
+++ resources/downloads/init.sh
@@ -24,35 +24,37 @@ if test -z "$BOARD" ; then
 fi
 
 # Create project directory
-mkdir -p $BOARD
-test -f "$BOARD/$BOARD.ioc" || $CURL $RAW/resources/downloads/$BOARD.ioc -o $BOARD/$BOARD.ioc 2>/dev/null
-test -f "$BOARD/$BOARD.ioc" || fail echo "Usupported board $BOARD"
+DIR=${BOARD}_mongoose
+DIR2=${BOARD}_dashboard_${DASH}
+test -d $DIR || mkdir -p $DIR
+test -f "$DIR/$BOARD.ioc" || $CURL $RAW/resources/downloads/$BOARD.ioc -o $DIR/$BOARD.ioc 2>/dev/null
+test -f "$DIR/$BOARD.ioc" || fail echo "Usupported board $BOARD"
 
 # Run CubeMX to generate project using a pre-configured .ioc file
-echo config load `pwd`/$BOARD/$BOARD.ioc > gen.mx
+echo config load `pwd`/$DIR/$BOARD.ioc > gen.mx
 echo project generate >> gen.mx
 echo exit >> gen.mx
-test -f "$BOARD/CMakeLists.txt" || $MX -q `pwd`/gen.mx
+test -f "$DIR/CMakeLists.txt" || $MX -q `pwd`/gen.mx
 
 # Adopt generated code for Mongoose
-MAIN_C=$BOARD/Core/Src/main.c
+MAIN_C=$DIR/Core/Src/main.c
 grep -q mongoose.h $MAIN_C || perl -i -ne 'print; print "#include \"mongoose.h\"\n" if /BEGIN Includes/' $MAIN_C
 grep -q '^int _write' $MAIN_C || perl -i -ne 'print; print "int _write(int fd, unsigned char *buf, int len) {\n  HAL_UART_Transmit(&huart3, buf, len, HAL_MAX_DELAY);\n  return len;\n}\n" if /USER CODE BEGIN 0/' $MAIN_C
 grep -q 'mg_mgr_init' $MAIN_C || perl -i -ne 'print; print "  struct mg_mgr mgr;\n  mg_mgr_init(&mgr);\n\n" if /USER CODE BEGIN WHILE/' $MAIN_C
 grep -q 'mg_mgr_poll' $MAIN_C || perl -i -ne 'print "    mg_mgr_poll(&mgr, 0);\n" if /USER CODE END WHILE/; print;' $MAIN_C
-test -f $BOARD/STM32H723XG_FLASH.ld && (grep -q eth_ram $BOARD/STM32H723XG_FLASH.ld || perl -i -ne 'print ; print "\n  /* Mongoose Ethernet driver */\n  .eth_ram : { *(.eth_ram .eth_ram*) } > RAM_D2 AT > FLASH\n" if /_sidata =/' $BOARD/STM32H723XG_FLASH.ld)
-test -f $BOARD/LICENSE || $CURL $RAW/LICENSE -o $BOARD/LICENSE
+test -f $DIR/STM32H723XG_FLASH.ld && (grep -q eth_ram $DIR/STM32H723XG_FLASH.ld || perl -i -ne 'print ; print "\n  /* Mongoose Ethernet driver */\n  .eth_ram : { *(.eth_ram .eth_ram*) } > RAM_D2 AT > FLASH\n" if /_sidata =/' $DIR/STM32H723XG_FLASH.ld)
+test -f $DIR/LICENSE || $CURL $RAW/LICENSE -o $DIR/LICENSE
 
 # If dashboard is specified, copy it to the project
 if test "$DASH" == "full" -o "$DASH" == "minimal" ; then
-  test -d ${BOARD}_${DASH} || cp -r $BOARD ${BOARD}_${DASH}
-  test -d ${BOARD}_${DASH}/Mongoose || mkdir -p ${BOARD}_${DASH}/Mongoose
-  test -f ${BOARD}_${DASH}/Mongoose/dashboard.c || $CURL $RAW/tutorials/device-dashboard/$DASH/dashboard.c -o ${BOARD}_${DASH}/Mongoose/dashboard.c
-  test -f ${BOARD}_${DASH}/Mongoose/dashboard.html || $CURL $RAW/tutorials/device-dashboard/$DASH/dashboard.html -o ${BOARD}_${DASH}/Mongoose/dashboard.html
-  test -f ${BOARD}_${DASH}/Mongoose/html2c.js || $CURL $RAW/resources/html2c.js -o ${BOARD}_${DASH}/Mongoose/html2c.js
-  MAIN2_C=${BOARD}_${DASH}/Core/Src/main.c
+  test -d $DIR2 || cp -r $DIR $DIR2
+  test -d $DIR2/Mongoose || mkdir -p $DIR2/Mongoose
+  test -f $DIR2/Mongoose/dashboard.c || $CURL $RAW/tutorials/device-dashboard/$DASH/dashboard.c -o $DIR2/Mongoose/dashboard.c
+  test -f $DIR2/Mongoose/dashboard.html || $CURL $RAW/tutorials/device-dashboard/$DASH/dashboard.html -o $DIR2/Mongoose/dashboard.html
+  test -f $DIR2/Mongoose/html2c.js || $CURL $RAW/resources/html2c.js -o $DIR2/Mongoose/html2c.js
+  MAIN2_C=$DIR2/Core/Src/main.c
   grep -q 'mg_dash_init' $MAIN2_C || perl -i -ne 'print; print "  mg_dash_init(&mgr);\n" if /mg_mgr_init/' $MAIN2_C
   grep -q 'mg_dash_poll' $MAIN2_C || perl -i -ne 'print; print "    mg_dash_poll(&mgr);\n" if /mg_mgr_poll/' $MAIN2_C
-  grep -q '^    Mongoose/file_data.c' ${BOARD}_${DASH}/CMakeLists.txt || perl -i -ne 'print; print "    Mongoose/dashboard.c\n    Mongoose/file_data.c\n" if /# Add user sources here/' ${BOARD}_${DASH}/CMakeLists.txt
-  grep -q 'html2c.js' ${BOARD}_${DASH}/CMakeLists.txt || perl -i -ne 'print "# Generate file_data.c from dashboard.html\nadd_custom_command(\n    OUTPUT \${CMAKE_SOURCE_DIR}/Mongoose/file_data.c\n    COMMAND node \${CMAKE_SOURCE_DIR}/Mongoose/html2c.js \${CMAKE_SOURCE_DIR}/Mongoose/dashboard.html -o \${CMAKE_SOURCE_DIR}/Mongoose/file_data.c\n    DEPENDS \${CMAKE_SOURCE_DIR}/Mongoose/dashboard.html\n    VERBATIM\n)\n\n" if /^# Add sources to executable/; print;' ${BOARD}_${DASH}/CMakeLists.txt
+  grep -q '^    Mongoose/file_data.c' $DIR2/CMakeLists.txt || perl -i -ne 'print; print "    Mongoose/dashboard.c\n    Mongoose/file_data.c\n" if /# Add user sources here/' $DIR2/CMakeLists.txt
+  grep -q 'html2c.js' $DIR2/CMakeLists.txt || perl -i -ne 'print "# Generate file_data.c from dashboard.html\nadd_custom_command(\n    OUTPUT \${CMAKE_SOURCE_DIR}/Mongoose/file_data.c\n    COMMAND node \${CMAKE_SOURCE_DIR}/Mongoose/html2c.js \${CMAKE_SOURCE_DIR}/Mongoose/dashboard.html -o \${CMAKE_SOURCE_DIR}/Mongoose/file_data.c\n    DEPENDS \${CMAKE_SOURCE_DIR}/Mongoose/dashboard.html\n    VERBATIM\n)\n\n" if /^# Add sources to executable/; print;' $DIR2/CMakeLists.txt
 fi
